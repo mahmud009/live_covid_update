135fdf6cf4d887afbb891303aebd08016c9a6b24
Mahmudur Rahman <mahmud.est@gmail.com>
2020-04-21 19:00:56 +0600

    Add files via upload

---
+++ precache-manifest.e9b2af864769e27399f128cf1ed42d5e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b670a261ddae59797d19558e51f75b22",
+    "url": "/live_covid_update/index.html"
+  },
+  {
+    "revision": "d101219bc02c64839cbd",
+    "url": "/live_covid_update/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "c199f0f2e65abc13d5e2",
+    "url": "/live_covid_update/static/css/main.89bfb23e.chunk.css"
+  },
+  {
+    "revision": "d101219bc02c64839cbd",
+    "url": "/live_covid_update/static/js/2.5e82822b.chunk.js"
+  },
+  {
+    "revision": "afb73ec7fcb39d0a5007be02e6943afe",
+    "url": "/live_covid_update/static/js/2.5e82822b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c199f0f2e65abc13d5e2",
+    "url": "/live_covid_update/static/js/main.cc18d532.chunk.js"
+  },
+  {
+    "revision": "7fcccf580c0220fe55ce",
+    "url": "/live_covid_update/static/js/runtime-main.1f00ba7b.js"
+  },
+  {
+    "revision": "84f1d79b733b0d9167b47c24586d01f5",
+    "url": "/live_covid_update/static/media/quarantine.84f1d79b.png"
+  },
+  {
+    "revision": "18407fba9a08b899307e521fd39e09f7",
+    "url": "/live_covid_update/static/media/shield.18407fba.png"
+  },
+  {
+    "revision": "7f1b657aefaff9aa80e290f3ec7e008a",
+    "url": "/live_covid_update/static/media/stay-home.7f1b657a.png"
+  }
+]);
